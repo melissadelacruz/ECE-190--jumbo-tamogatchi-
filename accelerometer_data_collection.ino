e115c66fdca59ccd40c0815f3050d1c4e6d0f2a9
Melissa De La Cruz <mdelacruz5445@gmail.com>
2026-02-25 12:26:03 -0800

    updated pins

--- accelerometer_data_collection.ino
+++ accelerometer_data_collection.ino
@@ -9,7 +9,7 @@ void setup() {
   Serial.begin(115200);
   
   // Custom I2C pins for ESP32-S3
-  Wire.begin(15, 16); //7 for scl and 8 for sda (clock and data)
+  Wire.begin(15, 16); //16 for scl and 15 for sda (clock and data)
 
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
