696424b7171686d6b5ffb4f3ef420b5f04eae453
Melissa De La Cruz <mdelacruz5445@gmail.com>
2026-03-23 03:41:46 -0700

    Comment out all code in testing_tft_spi.ino
    Comment out the entire code for testing purposes.

--- testing_tft_spi/testing_tft_spi.ino
+++ testing_tft_spi/testing_tft_spi.ino
@@ -1,39 +1,39 @@
-#include <Arduino_GFX_Library.h>
+// #include <Arduino_GFX_Library.h>
 
-// PIN MAPPING (Using the ones we discussed)
-#define TFT_SCK  7
-#define TFT_MOSI 6
-#define TFT_MISO -1 
-#define TFT_CS   5
-#define TFT_DC   4
-#define TFT_RST  15
+// // PIN MAPPING (Using the ones we discussed)
+// #define TFT_SCK  7
+// #define TFT_MOSI 6
+// #define TFT_MISO -1 
+// #define TFT_CS   5
+// #define TFT_DC   4
+// #define TFT_RST  15
 
-// Software SPI Bus - This bypasses the hardware that causes the "Panic"
-Arduino_DataBus *bus = new Arduino_SWSPI(
-    TFT_DC, TFT_CS, TFT_SCK, TFT_MOSI, TFT_MISO
-);
+// // Software SPI Bus - This bypasses the hardware that causes the "Panic"
+// Arduino_DataBus *bus = new Arduino_SWSPI(
+//     TFT_DC, TFT_CS, TFT_SCK, TFT_MOSI, TFT_MISO
+// );
 
-// ST7796 Driver for the 4.0" TN Panel
-Arduino_GFX *gfx = new Arduino_ST7796(
-    bus, TFT_RST, 1 /* Rotation */, false /* IPS */
-);
+// // ST7796 Driver for the 4.0" TN Panel
+// Arduino_GFX *gfx = new Arduino_ST7796(
+//     bus, TFT_RST, 1 /* Rotation */, false /* IPS */
+// );
 
-void setup() {
-  Serial.begin(115200);
-  delay(3000);
-  Serial.println("Starting Software SPI - No Hardware Hooks");
+// void setup() {
+//   Serial.begin(115200);
+//   delay(3000);
+//   Serial.println("Starting Software SPI - No Hardware Hooks");
 
-  if (!gfx->begin()) {
-    Serial.println("gfx->begin() failed!");
-  } else {
-    Serial.println("SUCCESS! Code is running.");
-    gfx->fillScreen(0x07E0); // Bright Green
-  }
-}
+//   if (!gfx->begin()) {
+//     Serial.println("gfx->begin() failed!");
+//   } else {
+//     Serial.println("SUCCESS! Code is running.");
+//     gfx->fillScreen(0x07E0); // Bright Green
+//   }
+// }
 
-void loop() {
-  gfx->fillScreen(0xF800); // Red
-  delay(1000);
-  gfx->fillScreen(0x001F); // Blue
-  delay(1000);
-}
+// void loop() {
+//   gfx->fillScreen(0xF800); // Red
+//   delay(1000);
+//   gfx->fillScreen(0x001F); // Blue
+//   delay(1000);
+// }
